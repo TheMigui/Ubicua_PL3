15db8777f5892a37191e3cdb80474a5290913916
TizianoBR <tizianobiondo@hotmail.com>
2025-12-18 19:20:02 +0100

    Nombre servlet correjido

--- app/src/main/java/com/example/apppecl3/ApiService.java
+++ app/src/main/java/com/example/apppecl3/ApiService.java
@@ -7,6 +7,6 @@ import retrofit2.http.GET;
 
 public interface ApiService {
 
-    @GET("UbicompServerExample/GetData")   // ruta del endpoint
+    @GET("Ubicua/GetStreets")   // ruta del endpoint
     Call<List<Street>> getItems();
 }
